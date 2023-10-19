b1ac5077d9b2c1aa441337ffb18c92545cf4b44d
Jiajun Dai <jdai10@mail.ccsf.edu>
2023-10-19 12:48:09 -0700

    first commit

---
+++ lambdas/getUser.js
@@ -0,0 +1,27 @@
+const Responses = require('./API_Responses');
+
+exports.handler = async event => {
+    console.log('event', event);
+
+    if (!event.pathParameters || !event.pathParameters.ID) {
+        // failed without an ID
+        return Responses._400({ message: 'missing the ID from the path' });
+    }
+
+    let ID = event.pathParameters.ID;
+
+    if (data[ID]) {
+        // return the data
+        return Responses._200(data[ID]);
+    }
+
+    //failed as ID not in the data
+    return Responses._400({ message: 'no ID in data' });
+};
+
+const data = {
+    1234: { firstName: 'Jiajun', lastName: 'Dai' },
+    7893: { firstName: 'Sarah', lastName: 'Yu' },
+    5132: { firstName: 'Walton', lastName: 'Ma' },
+    1234: { firstName: 'Yifu', lastName: 'Fang' },
+};
